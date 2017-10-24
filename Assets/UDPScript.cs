d13dbc5e98b64c92576be0857c0a21cd72bf428f
Peter Meyer <ptr_meyer@hotmail.com>
2017-10-24 17:15:08 +0200

    init

---
+++ Assets/UDPScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using System.Threading;
+
+public class UDPScript : MonoBehaviour {
+
+    private Text instruction;
+
+    private String message;
+
+    Thread receiveThread;
+    UdpClient client;
+
+    // -------------------------------------------------------------------------
+    public void Start()
+    {
+        message = "";
+        instruction = GetComponent<Text>();
+        instruction.text = "started";
+        //   init();
+        ReceiveData();
+    }
+
+    // -------------------------------------------------------------------------
+    private void init()
+    {
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        print("Start");
+    }
+
+    // -------------------------------------------------------------------------
+    private void ReceiveData()
+    {
+        int somePort = 11000;
+
+
+            client = new UdpClient(somePort);
+
+                // do stuff
+              //  instruction.text = "ddata";
+
+               
+
+                try
+                {
+                    client.BeginReceive(new AsyncCallback(recv), null);
+                }
+                catch (Exception e)
+                {
+              
+                    this.message = "Error: " + e.ToString();
+                }
+
+            
+
+        }
+
+
+
+    //CallBack
+    private void recv(IAsyncResult res)
+    {
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+        byte[] received = client.EndReceive(res, ref RemoteIpEndPoint);
+
+        //Process codes
+;
+        this.message = Encoding.UTF8.GetString(received);
+        client.BeginReceive(new AsyncCallback(recv), null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //  instruction.text = "ole " + UnityEngine.Random.Range(0, 10);
+        instruction.text = " " + message;
+
+    }
+    
+
+    void OnApplicationQuit()
+    {
+        receiveThread.Abort();
+        if (client != null)
+            client.Close();
+    }
+    
+
+
+}
